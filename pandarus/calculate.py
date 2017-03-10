c7d5189e4bf73d9aea9674af813fdabd52d8fe23
Chris Mutel <cmutel@gmail.com>
2017-03-10 18:44:37 +0100

    Add `rasterstats` method to Pandarus

--- pandarus/calculate.py
+++ pandarus/calculate.py
@@ -1,11 +1,15 @@
 # -*- coding: utf-8 -*-
-from .filesystem import json_exporter, get_appdirs_path
+from .conversion import check_type
+from .filesystem import json_exporter, get_appdirs_path, sha256
 from .maps import Map
 from .matching import MatchMaker, areal_calculation, intersection_calculation
+from .rasters import gen_zonal_stats
 from fiona.crs import from_string
-import fiona
-import os
 from shapely.geometry import mapping
+import datetime
+import fiona
+import json
+import os
 
 
 WGS84 = from_string("+datum=WGS84 +ellps=WGS84 +no_defs +proj=longlat")
@@ -15,6 +19,7 @@ class Pandarus(object):
     """Controller for all actions."""
     def __init__(self, from_filepath, to_filepath=None,
             from_metadata={}, to_metadata={}):
+        self.from_filepath = from_filepath
         self.from_map = Map(from_filepath, **from_metadata)
         self.metadata = {'first': {
             'sha256': self.from_map.hash,
@@ -28,6 +33,50 @@ class Pandarus(object):
                 'filename': os.path.basename(to_filepath)
             }
             self.metadata['second'].update(to_metadata)
+
+    def rasterstats(self, raster, filepath=None, band=1, compressed=True, **kwargs):
+        """Create statistics by matching ``raster`` against each spatial unit in ``self.from_map``.
+
+        * ``raster``: str. Filepath of the raster used for calculations.
+        * ``filepath``: str. Path of the results file to be created. Can be auto-generated.
+        * ``band``: int. Raster band used for calculations. Default is 1.
+        * ``compressed``: bool. Compress JSON results file.
+
+        Any additional ``kwargs`` are passed to ``gen_zonal_stats``.
+
+        """
+        assert check_type(raster) == 'raster'
+
+        if not filepath:
+            dirpath = get_appdirs_path("rasterstats")
+            filepath = os.path.join(
+                dirpath,
+                "{}-{}-{}.json".format(self.from_map.hash, sha256(raster), band)
+            )
+
+        if os.path.exists(filepath):
+            os.remove(filepath)
+
+        stats_generator = gen_zonal_stats(self.from_filepath, raster, band=band, **kwargs)
+        mapping_dict = self.from_map.get_fieldnames_dictionary(None)
+        results = [(mapping_dict[index], row)
+                   for index, row in enumerate(stats_generator)]
+
+        metadata = {
+            'vector': {
+                'sha256': self.from_map.hash,
+                'filepath': self.from_map.filepath,
+                'field': self.metadata['first']['field']
+            },
+            'raster': {
+                'sha256': sha256(raster),
+                'filepath': raster,
+                'band': band
+            },
+            'timestep': datetime.datetime.now().isoformat()
+        }
+        json_exporter(results, metadata, filepath, compressed)
+        return filepath
 
     def intersect(self, cpus=None, to_meters=True):
         if cpus != 0:
@@ -46,7 +95,7 @@ class Pandarus(object):
             )
         return self.data
 
-    def as_geojson(self, dct):
+    def as_feature(self, dct):
         mapping_from = self.from_map.get_fieldnames_dictionary(None)
         mapping_to = self.to_map.get_fieldnames_dictionary(None)
         for index, key in enumerate(dct):
@@ -61,20 +110,20 @@ class Pandarus(object):
             }
             yield gj
 
-    def intersection_file(self, filepath=None, cpus=None, pool=True):
+    def intersection_files(self, filepath=None, cpus=None, pool=True, driver='GeoJSON'):
         if not hasattr(self, 'to_map'):
             raise ValueError("Need ``to_map`` for intersection")
         if not filepath:
             dirpath = get_appdirs_path("intersections")
             filepath = os.path.join(
                 dirpath,
-                "{}-{}.geojson".format(*sorted([self.from_map.hash, self.to_map.hash]))
+                "{}-{}.".format(*sorted([self.from_map.hash, self.to_map.hash]) + driver.lower())
             )
 
         if os.path.exists(filepath):
             os.remove(filepath)
 
-        results = self.as_geojson(self.intersect(cpus, pool))
+        results = self.as_feature(self.intersect(cpus, pool))
 
         schema = {
             'properties': {
@@ -90,7 +139,7 @@ class Pandarus(object):
             with fiona.open(
                     filepath, 'w',
                     crs=WGS84,
-                    driver='GeoJSON',
+                    driver=driver,
                     schema=schema,
                 ) as sink:
                 for f in results:
@@ -144,7 +193,7 @@ class Pandarus(object):
     def export(self, filepath, compress=True):
         if not filepath.endswith("json"):
             filepath += ".json"
-        if isinstance(list(self.data.keys())[0], tuple):
+        if len(self.data) and isinstance(list(self.data.keys())[0], tuple):
             self.unpack_tuples()
         else:
             self.unpack_dictionary()
