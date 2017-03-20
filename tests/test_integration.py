bd8e8c20722e89898c4ea70eff07ad34376cf218
Chris Mutel <cmutel@gmail.com>
2017-03-20 13:22:22 +0100

    Add test for integer indices in intersections

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -16,6 +16,7 @@ dem = os.path.join(dirpath, "DEM.tif")
 outside = os.path.join(dirpath, "outside.geojson")
 remain_result = os.path.join(dirpath, "remaining.geojson")
 pgrid = os.path.join(dirpath, "grid-3410.geojson")
+igrid = os.path.join(dirpath, "grid-ints.geojson")
 psquare = os.path.join(dirpath, "square-3857.geojson")
 plines =  os.path.join(dirpath, "lines-25000.geojson")
 lines =  os.path.join(dirpath, "lines.geojson")
@@ -42,6 +43,57 @@ def test_intersection_polygon():
         for x, y, z in data['data']:
             assert x == 'by-myself'
             assert y in ('grid cell 1', 'grid cell 3')
+            assert np.isclose(z, area, rtol=1e-2)
+
+        assert data['metadata'].keys() == {'first', 'second', 'when'}
+        assert data['metadata']['first'].keys() == {'field', 'filename', 'path', 'sha256'}
+        assert data['metadata']['second'].keys() == {'field', 'filename', 'path', 'sha256'}
+
+        with fiona.open(vector_fp) as src:
+            meta = src.meta
+
+            assert meta['driver'] == 'GeoJSON'
+            assert meta['schema'] == {
+                'geometry': 'MultiPolygon',
+                'properties': dict([
+                    ('measure', 'float'),
+                    ('from_label', 'str'),
+                    ('id', 'int'),
+                    ('to_label', 'str')
+                ])
+            }
+            assert meta['crs'] == {'init': 'epsg:4326'}
+
+            coords = [
+                [[[(0.5, 1.5), (0.5, 2.0), (1.0, 2.0), (1.0, 1.5), (0.5, 1.5)]]],
+                [[[(1.5, 2.0), (1.5, 1.5), (1.0, 1.5), (1.0, 2.0), (1.5, 2.0)]]]
+            ]
+
+            for feature in src:
+                assert feature['geometry']['coordinates'] in coords
+                assert feature['geometry']['type'] == 'MultiPolygon'
+                assert feature['properties'].keys() == {'measure', 'from_label', 'to_label', 'id'}
+                assert np.isclose(feature['properties']['measure'], area, rtol=1e-2)
+
+def test_intersection_polygon_integer_indices():
+    area = 1/4 * (4e7 / 360) ** 2
+
+    with tempfile.TemporaryDirectory() as dirpath:
+        vector_fp, data_fp = intersect(
+            outside,
+            'name',
+            igrid,
+            'name',
+            dirpath=dirpath,
+            compress=False,
+            log_dir=dirpath
+        )
+        data = json.load(open(data_fp))
+
+        assert len(data['data']) == 2
+        for x, y, z in data['data']:
+            assert x == 'by-myself'
+            assert y in (1, 3)
             assert np.isclose(z, area, rtol=1e-2)
 
         assert data['metadata'].keys() == {'first', 'second', 'when'}
